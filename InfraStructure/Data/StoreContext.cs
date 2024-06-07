764e6daccdef25251d2ef9824badff523db54d16
DANYLOPC\goawr <Goawrh1av@gmail.com>
2024-06-07 10:07:54 +0200

    Added config for migrations

--- InfraStructure/Data/StoreContext.cs
+++ InfraStructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 
 
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,5 +16,12 @@ namespace Infastructure.Data
         public DbSet<ProductBrand> ProductBrands{ get; set; }
 
         public DbSet<ProductType> ProductTypes{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)  
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
     }
 }
